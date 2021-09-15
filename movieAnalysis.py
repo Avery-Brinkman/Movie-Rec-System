17502976775155349fd2c3c4df1f2d3b3e30854b
Avery Brinkman <brinkmae@mail.uc.edu>
2021-09-15 16:00:06 -0400

    None

--- movieAnalysis.py
+++ movieAnalysis.py
@@ -1,13 +1,4 @@
 from sklearn import tree
-from sklearn.datasets import load_iris
-
-iris = load_iris()
-
-x = iris.data
-y = iris.target
-
-clf  = tree.DecisionTreeClassifier()
-
-clf.fit(x, y)
-
-tree.plot_tree(clf)
+import graphviz
+import os 
+os.chdir('C:/Users/avery/Desktop') 
